cb3949e05f4fae4318d7ce388ae7387c07fe8ef8
Artem Burashnikov <artem.burashnikov@gmail.com>
2024-03-14 23:21:24 +0300

    feat: add poly_eq; update poly_init; formatting

--- src/poly.c
+++ src/poly.c
@@ -6,11 +6,11 @@
 poly_t *poly_init(int8_t degree, int8_t *coeff) {
   poly_t *poly = malloc(sizeof(*poly));
 
-  if (!poly || !coeff) {
+  if (!poly || !coeff || (degree < 0)) {
     return NULL;
   }
-  
-  poly->deg = (degree > 0) ? degree : MINUS_INFINITY;
+
+  poly->deg = degree;
   poly->coeff = coeff;
 
   return poly;
@@ -20,4 +20,19 @@ void poly_destroy(poly_t *poly) {
   free(poly->coeff);
   free(poly);
   return;
+}
+
+int poly_eq(poly_t *a, poly_t *b) {
+  if (!a || !b || (a->deg != b->deg)) {
+    return 0;
+  }
+
+  for (size_t i = 0; i < a->deg; ++i) {
+    if (a->coeff[i] == b->coeff[i]) {
+      continue;
+    }
+    return 0;
+  }
+
+  return 1;
 }
--- src/poly.h
+++ src/poly.h
@@ -6,12 +6,18 @@
 
 // Polynomial.
 typedef struct {
-  int8_t deg;    // Degree of polynomial.
-  int8_t *coeff; // Array of coefficients.
+  int8_t deg;     // Degree of polynomial.
+  int8_t *coeff;  // Array of coefficients.
 } poly_t;
 
-// Initialize a polynomial.
+/* Initialize a polynomial.
+   No checking is done to ensure that degree matches the array of coefficients.
+ */
 poly_t *poly_init(int8_t degree, int8_t *coeff);
 
 // Destroy a given polynomial.
 void poly_destroy(poly_t *poly);
+
+/* Check if two polynomials are equal.
+   Return 1 if the degree and corresponding coefficients match. */
+int poly_eq(poly_t *a, poly_t *b);
