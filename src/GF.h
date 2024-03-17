c6d174812cbb6a690c22db65aba6cbd01094300b
Artem Burashnikov <artem.burashnikov@gmail.com>
2024-03-17 09:40:39 +0300

    fix: GF.h interface

--- src/GF.h
+++ src/GF.h
@@ -6,8 +6,8 @@
 
 // Galois field.
 typedef struct {
-  int8_t p;   // Characteristic of the field GF(p).
-  int8_t n;   // Dimension of the field extension.
+  uint8_t p;   // Characteristic of the field GF(p).
+  uint8_t n;   // Dimension of the field extension.
   poly_t *I;  // Irreducible polynomial over GF(p)[X] of degree n.
 } GF_t;
 
@@ -18,45 +18,40 @@ typedef struct {
 } GF_elem_t;
 
 /* Initialize a quotient field. */
-GF_t *GF_init(int8_t p, int8_t n, poly_t *I);
+GF_t *GF_init(uint8_t p, uint8_t n, poly_t *I);
 
-/* Get an element over the given GF quotient field.
-   Provide an array of polynomial coefficients of the specified length. */
-GF_elem_t GF_elem_from_array(int8_t *coeff, uint8_t len, GF_t GF);
+/* Given an array of coefficients of the specified length and GF(p)[x]/(I),
+   Return an element over that field. */
+GF_elem_t *GF_elem_from_array(int8_t *coeff, uint8_t len, GF_t *GF);
 
 /* Destroy a given element of the Galois Field.
    The field itself is left untouched. */
 void GF_elem_destroy(GF_elem_t *a);
 
-/* Set coefficients mod p. */
-void GF_elem_normalize(GF_elem_t *a);
-
 /* Return 1 if the given fields are equal.
    Meaning they have the same characteristic and irreducible polynomials. */
 int GF_eq(const GF_t *f, const GF_t *k);
 
-/* res = a + b. Return 0 on success. */
+/* res = a + b mod (I). Return 0 on success. */
 int GF_elem_sum(GF_elem_t *res, GF_elem_t a, GF_elem_t b);
 
-/* res = a - b. Return 0 on success. */
+/* res = a - b mod (I). Return 0 on success. */
 int GF_elem_diff(GF_elem_t *res, GF_elem_t a, GF_elem_t b);
 
-/* res = a * b in GF(p)/(I). Return 0 on success. */
+/* res = a * b mod (I). Return 0 on success. */
 int GF_elem_prod(GF_elem_t *res, GF_elem_t a, GF_elem_t b);
 
-/* Find q and r over GF(p)[x]/(I) such that a = bq + r, where deg r < deg b.
-   One of q or r can be NULL, meaning the result for that variable won't
-   be stored. Return 0 on success. */
-int GF_elem_div(GF_elem_t *q, GF_elem_t *r, GF_elem_t a, GF_elem_t b);
+/* Calculate res: a = b * res mod (I). Return 0 on success. */
+int GF_elem_div(GF_elem_t *res, GF_elem_t a, GF_elem_t b);
+
+/* Calculate res = -a mod p. Return 0 on success. */
+int GF_elem_get_complement(GF_elem_t *res, GF_elem_t a);
+
+/* Calculate res: res * a = 1 mod (I). Return 0 on success. */
+int GF_elem_get_inverse(GF_elem_t *res, GF_elem_t a);
 
 /* Return neutral element of the given finite field. */
 GF_elem_t *GF_elem_get_neutral(GF_t *GF);
 
 /* Return unity element of the given finite field. */
 GF_elem_t *GF_elem_get_unity(GF_t *GF);
-
-/* Calculate res = -a mod p. Return 0 on success. */
-int GF_elem_get_complement(GF_elem_t *res, GF_elem_t a);
-
-/* Calculate res s.t. res * a = 1 mod (I) */
-int GF_elem_get_inverse(GF_elem_t *res, GF_elem_t a);
