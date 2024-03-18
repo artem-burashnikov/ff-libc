4765392fe8b1c50a61aa8026d120ce831f747fc9
Artem Burashnikov <artem.burashnikov@gmail.com>
2024-03-18 14:25:16 +0300

    refactor: gf diff

--- src/GF_diff.c
+++ src/GF_diff.c
@@ -10,14 +10,13 @@ int GF_elem_diff(GF_elem_t *res, GF_elem_t a, GF_elem_t b) {
   if (!res) {
     return 1;
   }
-  // Set b coefficients to their complements mod p.
-  GF_elem_get_complement(&b, b);
+  // Get b's complement.
+  GF_elem_t *negb = GF_elem_get_complement(b);
 
   // Now calculate res = a + ~b. res is normalized in sum.
-  GF_elem_sum(res, a, b);
+  GF_elem_sum(res, a, *negb);
 
-  // Complement b again.
-  GF_elem_get_complement(&b, b);
+  GF_elem_destroy(negb);
 
   return 0;
 }
