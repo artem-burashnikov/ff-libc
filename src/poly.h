e60e5018fe65da115a3aaab80e081fc7e5456c02
Artem Burashnikov <artem.burashnikov@gmail.com>
2024-03-18 14:23:00 +0300

    refactor: change type of poly fields to size_t and uint8_t

--- src/poly.h
+++ src/poly.h
@@ -5,13 +5,13 @@
 
 // Polynomial.
 typedef struct {
-  int8_t deg;     // Degree of polynomial.
+  uint8_t deg;    // Degree of polynomial.
   int8_t *coeff;  // Array of coefficients.
-  int8_t len;     // Length of the array of coefficients.
+  size_t len;     // Length of the array of coefficients.
 } poly_t;
 
 // Initialize a polynomial.
-poly_t *poly_from_array(int8_t deg, int8_t *coeff, int8_t len);
+poly_t *poly_from_array(uint8_t deg, int8_t *coeff, size_t len);
 
 // Destroy a given polynomial.
 void poly_destroy(poly_t *a);
@@ -22,18 +22,18 @@ void poly_destroy(poly_t *a);
 int poly_eq(const poly_t *a, const poly_t *b);
 
 /* Return a zero polynomial of the given length.*/
-poly_t *poly_create_zero(int8_t len);
+poly_t *poly_create_zero(size_t len);
 
 /* Set res = a + b, where a and b are polynomials over Fp. */
-int poly_carryless_sum(poly_t *res, poly_t a, poly_t b, int8_t p);
+void poly_carryless_sum(poly_t *res, poly_t a, poly_t b, int8_t p);
 
 /* Set a = a mod b, where a and b are polynomials over Fp.
    Assume deg a >= deg b.
    Notice that a's original coefficients are lost after the operation. */
-int poly_carryless_div(poly_t *a, poly_t b, int8_t p);
+void poly_carryless_div(poly_t *a, poly_t b, int8_t p);
 
 /* Return a new polynomial which is a * b mod (I). */
-int poly_carryless_mul(poly_t *res, poly_t a, poly_t b, poly_t I, int8_t p);
+void poly_carryless_mul(poly_t *res, poly_t a, poly_t b, int8_t p);
 
 /* Normalize the degree (find the greatest non zero coefficient index)
    of the given polynomial. */
