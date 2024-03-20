4fa2976ac589194cdc8897f90ba316a72026f47c
Artem Burashnikov <artem.burashnikov@gmail.com>
2024-03-20 03:21:56 +0300

    refactor: remove redundant helper function

--- src/utils.h
+++ src/utils.h
@@ -9,8 +9,5 @@ uint8_t complement(uint8_t a, uint8_t p);
 
 int8_t inverse(int8_t a, int8_t p);
 
-// Find q such that y*q = x mod p.
-uint8_t x_div_y_mod_p(uint8_t x, uint8_t y, uint8_t p);
-
 // Fast base to the exponent.
 uint64_t fpow(uint8_t base, uint8_t exp);
